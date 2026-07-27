3efa73da0369e8536635fab4d65932afe3bae428
Martín Gaspar Straus <martinstraus@gmail.com>
2026-07-27 20:07:56 -0300

    Scaffolding for rendering units with shapes.

--- world.cpp
+++ world.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <GL/freeglut.h>
+#include <vector>
+
+// General purpose types
 
 template <typename T>
 class Point {
@@ -25,15 +28,69 @@ public:
     T height() { return this->_height; }
 };
 
+// World types
+
+class Shape {
+
+public:
+    virtual ~Shape() = default;
+    virtual void draw(Point<float> location) {
+        std::cout << "[draw not implemented]\n";
+    }
+};
+
+class Square : public Shape {
+private:
+    Size<float> _size;
+
+public:
+    Square(Size<float> size) : _size(size) {}
+
+    void draw(Point<float> location) override {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glColor3f(1, 0, 0);
+        glBegin(GL_QUADS);
+        glVertex2f(-0.2, -0.2);
+        glVertex2f( 0.2, -0.2);
+        glVertex2f( 0.2,  0.2);
+        glVertex2f(-0.2,  0.2);
+        glEnd();
+    }
+};
+
+class Unit {
+private:
+    // Unit does not own this pointer; the caller must keep the Shape alive.
+    Shape* _shape;
+    Point<float> _location;
+
+public:
+    Unit(Shape* shape, Point<float> location) : _shape(shape), _location(location) {}
+    void draw() {
+        this->_shape->draw(this->_location);
+    }    
+};
+
 class World {
 private:
     Size<int> _size;
+    std::vector<Unit> _units;
 
 public:
-    World(Size<int> size): _size(size) {}
+    World(Size<int> size): _size(size) { }
     ~World();
     Size<int> size() { return this->_size; };
+    void addUnit(Unit unit) {
+        _units.push_back(unit);
+    }
+    void render() {
+        for (Unit& unit : _units) {
+            unit.draw();
+        } 
+    }
 };
+
+// Graphics types
 
 class Screen {
 private:
@@ -46,15 +103,10 @@ public:
 
 };
 
+World* world;
+
 void display() {
-    glClear(GL_COLOR_BUFFER_BIT);
-    glColor3f(1, 0, 0);
-    glBegin(GL_QUADS);
-    glVertex2f(-0.2, -0.2);
-    glVertex2f( 0.2, -0.2);
-    glVertex2f( 0.2,  0.2);
-    glVertex2f(-0.2,  0.2);
-    glEnd();
+    world->render();
     glutSwapBuffers();
 }
 
@@ -72,6 +124,13 @@ int main(int argc, char** argv) {
         (float)(screenSize.width() - windowSize.width()) / 2,
         (float)(screenSize.height() - windowSize.height()) / 2
     }; 
+
+    Size<int> worldSize{1024, 768};
+    world = new World(worldSize);
+    Square square(Size<float>(10.0f, 10.0f));
+    Unit unit(&square, Point<float>(50.0f, 50.0f));
+    world->addUnit(unit);
+
     
     glutInitWindowSize(windowSize.width(), windowSize.height());
     glutInitWindowPosition(windowPosition.x(), windowPosition.y());
@@ -84,6 +143,7 @@ int main(int argc, char** argv) {
     glLoadIdentity();
     glOrtho(-1, 1, -1, 1, -1, 1);
     glutDisplayFunc(display);
+
     glutMainLoop();
     return 0;
 }
